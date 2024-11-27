02ab038b49d9509bc2fda4a3019e618b128c709f
zavdav <157126752+zavdav@users.noreply.github.com>
2024-11-27 18:13:26 +0100

    Don't kick sender

--- src/main/kotlin/org/betamc/core/commands/CommandKickAll.kt
+++ src/main/kotlin/org/betamc/core/commands/CommandKickAll.kt
@@ -2,6 +2,7 @@ package org.betamc.core.commands
 
 import org.betamc.core.config.Language
 import org.bukkit.Bukkit
+import org.bukkit.entity.Player
 import org.poseidonplugins.commandapi.Command
 import org.poseidonplugins.commandapi.CommandEvent
 import org.poseidonplugins.commandapi.colorize
@@ -16,7 +17,9 @@ class CommandKickAll : Command(
 
     override fun execute(event: CommandEvent) {
         for (player in Bukkit.getOnlinePlayers()) {
-            player.kickPlayer(if (event.args.size > 1) colorize(joinArgs(event.args, 0)) else Language.KICK_DEFAULT_MESSAGE.msg)
+            if (event.sender !is Player || !player.equals(event.sender as Player)) {
+                player.kickPlayer(if (event.args.isNotEmpty()) colorize(joinArgs(event.args, 0)) else Language.KICK_DEFAULT_MESSAGE.msg)
+            }
         }
     }
 }
