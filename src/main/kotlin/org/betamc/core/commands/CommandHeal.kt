3bb58a3c33cd283030e95e6e9e9cbeae832843d0
zavdav <157126752+zavdav@users.noreply.github.com>
2024-12-04 14:30:27 +0100

    Change heal logic

--- src/main/kotlin/org/betamc/core/commands/CommandHeal.kt
+++ src/main/kotlin/org/betamc/core/commands/CommandHeal.kt
@@ -1,7 +1,7 @@
 package org.betamc.core.commands
 
 import org.betamc.core.config.Language
-import org.bukkit.Bukkit
+import org.betamc.core.util.Utils
 import org.bukkit.entity.Player
 import org.poseidonplugins.commandapi.Command
 import org.poseidonplugins.commandapi.CommandEvent
@@ -13,34 +13,34 @@ class CommandHeal : Command(
     description = "Heals a player to full health.",
     usage = "/heal [player]",
     permission = "bmc.heal",
+    isPlayerOnly = true,
     maxArgs = 1,
     preprocessor = Preprocessor()) {
 
     override fun execute(event: CommandEvent) {
-        if (event.args.isEmpty()) {
-            if (event.sender !is Player) {
-                sendMessage(event.sender, Language.PLAYER_ONLY)
-                return
-            }
-
-            (event.sender as Player).health = 20
-            sendMessage(event.sender, Language.HEAL_SELF)
-        } else if (hasPermission(event.sender, "bmc.heal.others")) {
-            val player: Player? = Bukkit.matchPlayer(event.args[0]).getOrNull(0)
+        var player: Player? = event.sender as Player
+        if (event.args.isNotEmpty()) {
+            player = Utils.getPlayerFromUsername(event.args[0])
             if (player == null) {
                 sendMessage(event.sender, Language.PLAYER_NOT_FOUND.msg
                     .replace("%player%", event.args[0]))
                 return
             }
+        }
 
-            player.health = 20
-            if (!event.sender.name.equals(player.name, true)) {
-                sendMessage(event.sender, Language.HEAL_PLAYER.msg
-                    .replace("%player%", player.name))
-            }
-            sendMessage(player, Language.HEAL_SELF)
-        } else {
+        val isSelf = (event.sender as Player).uniqueId == player!!.uniqueId
+        if (!isSelf && !hasPermission(event.sender, "bmc.heal.others")) {
             sendMessage(event.sender, Language.NO_PERMISSION)
+            return
+        }
+        player.health = 20
+
+        if (isSelf) {
+            sendMessage(event.sender, Language.HEAL_SELF)
+        } else {
+            sendMessage(event.sender, Language.HEAL_PLAYER.msg
+                .replace("%player%", player.name))
+            sendMessage(player, Language.HEAL_SELF)
         }
     }
 }
