d27a6e7882008d45491191e834f0806d016c0aec
zavdav <157126752+zavdav@users.noreply.github.com>
2024-12-21 23:34:59 +0100

    Change variable name

--- src/main/kotlin/org/betamc/core/commands/CommandHeal.kt
+++ src/main/kotlin/org/betamc/core/commands/CommandHeal.kt
@@ -18,27 +18,27 @@ class CommandHeal : Command(
     preprocessor = Preprocessor()) {
 
     override fun execute(event: CommandEvent) {
-        var player: Player? = event.sender as Player
+        var target: Player? = event.sender as Player
         if (event.args.isNotEmpty()) {
-            player = Utils.getPlayerFromUsername(event.args[0])
-            if (player == null) {
+            target = Utils.getPlayerFromUsername(event.args[0])
+            if (target == null) {
                 sendMessage(event.sender, Utils.format(Language.PLAYER_NOT_FOUND, event.args[0]))
                 return
             }
         }
 
-        val isSelf = (event.sender as Player).uniqueId == player!!.uniqueId
+        val isSelf = (event.sender as Player).uniqueId == target!!.uniqueId
         if (!isSelf && !hasPermission(event.sender, "bmc.heal.others")) {
             sendMessage(event.sender, Language.NO_PERMISSION)
             return
         }
-        player.health = 20
+        target.health = 20
 
         if (isSelf) {
             sendMessage(event.sender, Utils.format(Language.HEAL_SUCCESS, "You have"))
         } else {
-            sendMessage(event.sender, Utils.format(Language.HEAL_SUCCESS, "${player.name} has"))
-            sendMessage(player, Utils.format(Language.HEAL_SUCCESS, "You have"))
+            sendMessage(event.sender, Utils.format(Language.HEAL_SUCCESS, "${target.name} has"))
+            sendMessage(target, Utils.format(Language.HEAL_SUCCESS, "You have"))
         }
     }
 }
