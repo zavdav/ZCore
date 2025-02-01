4c8c1123bc5128e58da1ccadd995830df000c03c
zavdav <157126752+zavdav@users.noreply.github.com>
2025-02-01 11:55:01 +0100

    Setup multi-module project in preparation for ZImport

---
+++ ZImport/src/main/kotlin/org/poseidonplugins/zcore/ZImport.kt
@@ -0,0 +1,14 @@
+package org.poseidonplugins.zcore
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ZImport : JavaPlugin() {
+
+    override fun onEnable() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDisable() {
+        TODO("Not yet implemented")
+    }
+}
