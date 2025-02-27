7c59cc3a4d2776433d3960afa9c0d32a60964db1
zavdav <157126752+zavdav@users.noreply.github.com>
2025-02-27 14:51:25 +0100

    Make /zcore display fancy ascii art

--- src/main/kotlin/me/zavdav/zcore/commands/CommandZCore.kt
+++ src/main/kotlin/me/zavdav/zcore/commands/CommandZCore.kt
@@ -1,6 +1,9 @@
 package me.zavdav.zcore.commands
 
 import me.zavdav.zcore.ZCore
+import me.zavdav.zcore.util.getMessage
+import me.zavdav.zcore.util.send
+import me.zavdav.zcore.util.sendTl
 import org.poseidonplugins.commandapi.CommandEvent
 
 class CommandZCore : ZCoreCommand(
@@ -11,8 +14,10 @@ class CommandZCore : ZCoreCommand(
 ) {
 
     override fun execute(event: CommandEvent) {
-        val desc = ZCore.INSTANCE.description
-        event.sender.sendMessage("§e${desc.name} v${desc.version}")
-        event.sender.sendMessage("§eType /help for a list of commands.")
+        val asciiArt = getMessage("zcoreAsciiArt")
+        for (line in asciiArt.split("\n")) {
+            event.sender.send(line)
+        }
+        event.sender.sendTl("zcoreVersion", "version" to ZCore.INSTANCE.description.version)
     }
 }
